bbcc2ff80971b4e73b41a91e4c848fdc15204a1b
Scott Feichter <scottfeichter@gmail.com>
2024-05-13 19:09:35 -0700

    second draft Asset class

--- asset.js
+++ asset.js
@@ -1,4 +1,13 @@
-function getTimeStamp() {
+let ASSET_NUMBER = 0;
+const ASSETS_IN_EXISTENCE = [];
+
+function getAssetNumer() {
+  const assetNumber = ASSET_NUMBER;
+  ASSET_NUMBER++;
+  return assetNumber;
+}
+
+function getTimeStamp(assetNumber) {
   const timeStamp = {};
   const timeCreated = new Date();
 
@@ -9,6 +18,7 @@ function getTimeStamp() {
   timeStamp.minute = timeCreated.getMinutes();
   timeStamp.second = timeCreated.getSeconds();
   timeStamp.milliseconds = timeCreated.getMilliseconds();
+  timeStamp.assetNumber = assetNumber;
 
   timeStamp.serial =
     timeStamp.month.toString() +
@@ -23,7 +33,9 @@ function getTimeStamp() {
     "." +
     timeStamp.second.toString() +
     "." +
-    timeStamp.milliseconds.toString();
+    timeStamp.milliseconds.toString() +
+    "." +
+    timeStamp.assetNumber;
 
   timeStamp.GMT = timeCreated;
 
@@ -31,10 +43,37 @@ function getTimeStamp() {
 }
 
 class Asset {
-  constructor(initialValueUSD = 0, timeStamp = getTimeStamp()) {
-    this.initialValueUSD = initialValueUSD;
-    this.timeStamp = timeStamp;
+  constructor(assetInitialValueUSD = 0, assetName = "unnamedAsset") {
+    this.assetInitialValueUSD = assetInitialValueUSD;
+    this.assetName = assetName;
+    this.assetNumber = getAssetNumer();
+    this.assetTimeStamp = getTimeStamp(this.assetNumber);
+    this.assetCurrentValue = assetInitialValueUSD;
   }
 }
 
+function checkIfAsset(currentValue) {
+  return Math.sign(currentValue) >= 0;
+}
 
+function createAsset(assetInitialValueUSD, assetName) {
+  if (checkIfAsset(assetInitialValueUSD)) {
+    const asset = new Asset(assetInitialValueUSD, assetName);
+    ASSETS_IN_EXISTENCE.push(asset);
+    console.log(`Asset Number ${ASSET_NUMBER - 1} successfully created`);
+    return `Asset Number ${ASSET_NUMBER - 1} successfully created`;
+  } else {
+    console.log(`Asset Number ${ASSET_NUMBER} not created:
+    asset value must be a number greater than or equal to zero.`);
+    return `Asset Number ${ASSET_NUMBER} not created:
+    asset value must be a number greater than or equal to zero.`;
+  }
+}
+
+createAsset(1, "dollar");
+createAsset(-1);
+createAsset(1);
+
+
+console.log(`Number of Assets in existence: `, ASSETS_IN_EXISTENCE.length);
+console.log(ASSETS_IN_EXISTENCE[0]);
